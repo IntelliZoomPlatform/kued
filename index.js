60cb1c6a1d9b65756045e8b94e29fe8ed2c69c98
Richard Clayton <rlc.blacksun@gmail.com>
2016-06-08 15:39:45 -0700

    Hapi integration, exporting models, bump version.

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+module.exports.BridgeFactory = require('./lib/bridge-factory');
+module.exports.Checkpoint = require('./lib/checkpoint');
+module.exports.CronWorker = require('./lib/cronworker');
+module.exports.DependencyManager = require('./lib/dependency-manager');
+module.exports.Hapi = require('./lib/integrations/hapi');
+module.exports.TaskManager = require('./lib/task-manager');
+module.exports.Worker = require('./lib/worker');
+module.exports.WorkerFactory = require('./lib/worker-factory');
+module.exports.Workgroup = require('./lib/workgroup');
+module.exports.WorkgroupManager = require('./lib/workgroup-manager');
